8dde36cb6476882a68aead23f23c4d2f7aa323ef
ScottTomassetti <scott_t_ct@hotmail.com>
2022-01-18 03:55:37 -0500

    Final commit

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,11 +1,11 @@
-import { Route, useParams, useRouteMatch } from "react-router-dom"
+import { Route, useParams, Routes } from "react-router-dom"
 
 // nested routes
 import Offers from "./Offers"
 
 export default function ProductDetails() {
   const { id } = useParams()
-  const { path } = useRouteMatch()
+  
 
   return (
     <div className="content">
@@ -20,9 +20,9 @@ export default function ProductDetails() {
         </div>
       </div>
 
-      <Route path={`${path}/offers`}>
-        <Offers />
-      </Route>
+      <Routes>
+        <Route path="offers" element={<Offers/>}/>
+      </Routes>
     </div>
   )
 }
